b829af69a9f0c3929a5cff312572bd51319ab731
semye <464622832@qq.com>
2021-07-09 21:53:22 +0800

    commit

--- src/main/kotlin/com/semye/kotlin/generics/AComponent.kt
+++ src/main/kotlin/com/semye/kotlin/generics/AComponent.kt
@@ -1,8 +1,5 @@
 package com.semye.kotlin.generics
 
-import com.semye.kotlin.generics.AModel
-import com.semye.kotlin.generics.BaseComponent
-
 class AComponent : BaseComponent<AModel>() {
     override fun setDataModel(data: AModel) {
         TODO("Not yet implemented")
--- src/main/kotlin/com/semye/kotlin/generics/BModel.kt
+++ src/main/kotlin/com/semye/kotlin/generics/BModel.kt
@@ -1,6 +1,4 @@
 package com.semye.kotlin.generics
 
-import com.semye.kotlin.generics.BaseModel
-
 class BModel : BaseModel() {
 }
