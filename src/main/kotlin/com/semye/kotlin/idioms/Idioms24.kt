e47f7cadc1d01aa66698016b5efd9609ee3b23af
yesheng <464622832@qq.com>
2023-07-25 01:03:14 +0800

    commit

--- src/main/kotlin/com/semye/kotlin/idioms/Idioms24.kt
+++ src/main/kotlin/com/semye/kotlin/idioms/Idioms24.kt
@@ -7,10 +7,10 @@ package com.semye.kotlin.idioms
  * Configure properties of an object (apply)
  */
 fun main() {
-    val myRectangle = Rectangle().apply {
-        length = 4
-        breadth = 5
-        color = 0xFAFAFA
-    }
+//    val myRectangle = Rectangle().apply {
+//        length = 4
+//        breadth = 5
+//        color = 0xFAFAFA
+//    }
 }
 
